a56294769572a0ba2d4e88da5bcdf149615561ba
Anna Kiryanova <annetamiants@yandex.ru>
2023-07-16 18:10:37 +0300

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 48797;
+        int value = 20;
+        int bonus = cost / value;
+        System.out.println(bonus + " Бонусных миль");
+    }
+}
